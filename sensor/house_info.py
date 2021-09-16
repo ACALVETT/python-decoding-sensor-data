7f484f5d3cbb47878b5f583b9ef09d42ac109120
ACALVETT <andrew@calvett.co.uk>
2021-09-16 12:02:12 +0100

    Update house_info.py

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -1,16 +1,14 @@
-class houseinfo():
+class HouseInfo():
 
     def __Init__(self,data):
 
-        data = self.data
+        self.data = data
 
     def get_data_by_area(self,field,rec_area = 0):
         field_data = []
         for record in self.data:
-            if rec_area = 0:
+            if rec_area == 0:
                 field_data.append(record[field])
-            #elif rec_area = int(record['area']):
+            elif rec_area == int(record['area']):
                 field_data.append(record[field])
-    field_data
-
-
+        field_data
