aeb28a2ffcd0c3fe7340ce04a9c06379ef67f61a
ACALVETT <andrew@calvett.co.uk>
2021-09-16 10:51:51 +0100

    Update load_data.py

---
+++ sensor/load_data.py
@@ -0,0 +1,7 @@
+import os,glob,csv
+
+def load_sensor_data():
+    sensor_data = []
+    sensor_files = glob.glob(os.path.join(os.getcwd(),"datasets","*.csv"))
+    for sensor_file in sensor_files:
+        with sensor_file
